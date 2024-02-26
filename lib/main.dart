b1adccd72b8d58849979f883088ea93f30433a7a
kelvinthuo999 <thuokelvinngigi@gmail.com>
2024-02-26 17:58:01 +0300

    create: new nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,18 @@
 import 'package:flutter/material.dart';
+import 'nav_bar.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Kazi Nasi',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyNavBar(),
+    );
+  }
+}
---
+++ lib/nav_bar.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class MyNavBar extends StatefulWidget {
+  @override
+  _MyNavBarState createState() => _MyNavBarState();
+}
+
+class _MyNavBarState extends State<MyNavBar> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Kazi Nasi'),
+        shadowColor: Colors.blue,
+      ),
+      body: Center(
+        child: _selectedIndex == 0
+            ? Text('Home Page')
+            : Text('Settings Page'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
